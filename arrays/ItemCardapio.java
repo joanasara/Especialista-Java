87ce76217f4373b8724cdd591d9aac7ca9e5d5ac
Joana Sara <joana.leite@aluno.unifametro.edu.br>
2025-01-01 18:40:33 -0300

    Desafio de ArrayList

--- arrays/ItemCardapio.java
+++ arrays/ItemCardapio.java
@@ -1,4 +1,15 @@
 package arrays;
 
 public class ItemCardapio {
+
+    String descricao;
+    double preco;
+
+    boolean possuiPrecoEntre(double precoMinimo, double precoMaximo) {
+        return preco >= precoMinimo && preco <= precoMaximo;
+    }
+
+    void imprimir() {
+        System.out.printf("%s x R$%.2f%n", descricao, preco);
+    }
 }
